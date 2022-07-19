b3bc58f78643cd6ccb87d754c7f8c1003aa92e29
LEANNA LUCAS <LEANNALUCAS10@GMAIL.COM>
2022-07-19 16:54:27 +0100

    add map images

--- src/App.js
+++ src/App.js
@@ -22,26 +22,38 @@ class App extends React.Component {
     const API = `https://eu1.locationiq.com/v1/search.php?key=${process.env.REACT_APP_CITY_EXPLORER_TOKEN}&q=${this.state.searchQuery}&format=json`;
     const res = await axios.get(API);
     console.log(API);
-    this.setState({ location: res.data[0] });
+    this.setState({
+      location: res.data[0],
+      lat: res.data[0].lat,
+      lon: res.data[0].lon,
+    });
     console.log(res.data);
   };
 
-  
+  getMap = async () => {
+    const API = `https://maps.locationiq.com/v3/staticmap?key=${process.env.REACT_APP_CITY_EXPLORER_TOKEN}&q=${this.state.searchQuery}&center=${this.state.lat},${this.state.lon}&zoom=1`;
+    const res = await axios.get(API);
+    console.log(API)
+    console.log(res);
+    this.setState({
+      map:res.data
+     })
+  };
 
   render() {
     return (
       <>
         <input onChange={this.handleChange} placeholder="Explore!" />
-        <button onClick={this.getLocation}>
-          Search
-        </button>
-
+        <button onClick={this.getLocation}>Search</button>
+        <button onClick={this.getMap}>Map</button>
         {this.state.location.display_name && (
           <h2>
-            This Yo! Sushi is in: {this.state.location.display_name}{" "}
+            City: {this.state.location.display_name}{" "}
             {this.state.location.lat} {this.state.location.lon}
           </h2>
         )}
+        {this.state.map}
+        
       </>
     );
   }
